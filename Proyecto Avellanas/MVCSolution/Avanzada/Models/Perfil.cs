ddf399158de4b7762d35dd482746fab6c20863d6
Jesus Martinez <yisustkd18@gmail.com>
2018-03-25 17:46:38 -0600

    Perfil Controller added

---
+++ Proyecto Avellanas/MVCSolution/Avanzada/Models/Perfil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Avanzada.Models
+{
+    public class Perfil
+    {
+        public string Nombre_Completo { get; set; }
+        public int id { get; set; }
+        public int Telefono{ get; set; }
+        public int Cedula { get; set; }
+        public int Carne { get; set; }
+        public int Fecha_Nac{ get; set; }
+        public int correo { get; set; }
+        public int sexo { get; set; }
+        
+    }
+}
